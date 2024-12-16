03adf3c8ecc8982e74eb1c10a2ef186b67f84636
Sushanth Reddy Kasireddy <144163287+Sushanth08k@users.noreply.github.com>
2024-12-16 23:29:03 +0000

    datatypes

--- 01_Basics/02_datatypes.js
+++ 01_Basics/02_datatypes.js
@@ -3,3 +3,13 @@ let name="sushanth"
 let age=19
 let isloggedIn=false
 console.log(typeof null);
+
+//Dates
+
+let myDate= new Date()
+console.log(myDate.toString())
+console.log(myDate.toDateString())
+console.log(myDate.toISOString())
+console.log(myDate.toLocaleDateString())
+console.log(myDate.toLocaleTimeString())
+console.log(myDate.toTimeString())
