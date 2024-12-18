9073e0127418009e8b3e910e0c91f9e032a93f3b
Sushanth Reddy Kasireddy <144163287+Sushanth08k@users.noreply.github.com>
2024-12-18 10:32:20 +0000

    objects

--- 01_Basics/04_objects.js
+++ 01_Basics/04_objects.js
@@ -23,3 +23,20 @@ jsuser.greeting=function(){
     console.log(`Hello Js user,this is ${this.name}`)
 }
 console.log(jsuser.greeting())
+
+
+//const tinderUser=new Object() Singleton object
+
+// const tinderUser = {};
+// tinderUser.id = "123abc";
+// console.log(tinderUser)
+
+const obj1={1:"a",2:"b"}
+const obj2={3:"c",4:"d"}
+// const obj3=Object.assign({},obj1,obj2)
+const obj3 ={...obj1,...obj2}
+console.log(obj3)
+console.log(Object.keys(obj3))
+console.log(Object.values(obj3))
+console.log(Object.entries(obj3))
+console.log(obj3.hasOwnProperty('3'))
