4aae5d293fee5e0561bfcfa96a85b9fe2d5f6283
Sushanth Reddy Kasireddy <144163287+Sushanth08k@users.noreply.github.com>
2025-01-05 09:35:20 +0000

    tictactoe

--- Tic_Tac_toe/app.js
+++ Tic_Tac_toe/app.js
@@ -37,21 +37,32 @@ const showWinner =(winner)=>{
     msgContainer.classList.remove("hide");
     disableBoxes();
 }
+const tie=()=>{
+    Msg.innerText="Match Drawn";
+    msgContainer.classList.remove("hide");
+    disableBoxes();
+}
 const checkwinner=()=>{
+    let flag=true;
+    let filledBoxes=0;
     for(let pattern of winPatterns){
         let pos1val=boxes[pattern[0]].innerText;
         let pos2val=boxes[pattern[1]].innerText;
         let pos3val=boxes[pattern[2]].innerText;
-        if (pos1val!="" && pos2val!="" &&pos3val!=""){
-            if (pos1val===pos2val && pos2val===pos3val){
-                console.log("winner", pos1val);
-
-                showWinner(pos1val);
-            } 
+        if (pos1val !== "" && pos1val === pos2val && pos2val === pos3val){
+            console.log("winner", pos1val);
+            flag=false;
+            showWinner(pos1val);
         }
     };
-    
-
+    boxes.forEach(box => {
+        if (box.innerText !== "") {
+            filledBoxes++;
+        }
+    });
+    if (filledBoxes === 9 && flag) {
+        tie();
+    }
 }
 const resetGame=()=>{
     turnO=true;
